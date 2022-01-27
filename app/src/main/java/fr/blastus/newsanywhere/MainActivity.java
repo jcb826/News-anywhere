c93dab742d39dbdad77bba293003711b31350938
jc <SM@shed31>
2022-01-27 12:58:33 +0100

    Implementing fragments

--- app/src/main/java/fr/blastus/newsanywhere/MainActivity.java
+++ app/src/main/java/fr/blastus/newsanywhere/MainActivity.java
@@ -1,14 +1,26 @@
 package fr.blastus.newsanywhere;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    TabItem mhome,mscience,mhealth,mtech,mentertainment,msports;
+    PagerAdapter pagerAdapter;
+    Toolbar mtoolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mtoolbar=findViewById(R.id.toolbar);
+
     }
 }
